2fb713ea7abcf845f70e5f6ae69a9f54b7383e08
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-06-18 16:05:28 -0400

    add: se crea test unit inicial API

---
+++ src/test/java/cl/inacap/pipelinepredict/PipelinePredictApiApplicationTests.java
@@ -0,0 +1,12 @@
+package cl.inacap.pipelinepredict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinePredictApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
