40f09dd9fe7bec492666bdac0eeffa7f3f4382bb
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-06-18 15:55:07 -0400

    add: se crea Clase principal API

---
+++ src/main/java/cl/inacap/pipelinepredict/PipelinePredictApiApplication.java
@@ -0,0 +1,12 @@
+package cl.inacap.pipelinepredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinePredictApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelinePredictApiApplication.class, args);
+    }
+}
